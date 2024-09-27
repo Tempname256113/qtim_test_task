7820d8b18830d3b1cc10b64945bbaac213518c73
Alexandr Smolnikov <smolnikov.456@mail.ru>
2024-09-27 18:48:03 +0300

    feat: update main, update app module, add cookies and swagger config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import pgConfig from '../config/pg-config';
+import { UserModule } from './user/user.module';
+import redisConfig from '../config/redis.config';
+import { RedisModule } from '../redis/redis.module';
+import appConfig from '../config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [pgConfig, redisConfig, appConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('PG_CONFIG.host'),
+        port: config.get('PG_CONFIG.port'),
+        username: config.get('PG_CONFIG.user'),
+        password: config.get('PG_CONFIG.password'),
+        database: config.get('PG_CONFIG.dbName'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    RedisModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { JwtTokensService } from '../jwt-tokens/jwt-tokens.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api/v1');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Qtim test task api')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
+    .addCookieAuth(JwtTokensService.refreshTokenCookieTitle, {
+      type: 'http',
+      in: 'cookie',
+    })
+    .build();
+  const swagger = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api/v1/docs', app, swagger, {
+    customSiteTitle: 'qtim test task api',
+  });
+
+  const port = 3001;
+  await app.listen(port);
+
+  console.log(`app started on: http://localhost:${port}/api/v1`);
 }
 bootstrap();
